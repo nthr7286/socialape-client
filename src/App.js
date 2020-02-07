eb2ca41fef2526a505e8636bee779ce3ca849a38
Natsuki <nthr7286@gmail.com>
2020-02-07 17:14:40 +0900

    Add Scream Card Details

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {
   Route,
   Switch
 } from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/core/styles'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 import { Navbar } from './components'
 import {
   home,
@@ -12,21 +14,40 @@ import {
 } from './pages'
 import './App.css'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff',
+    }
+  }
+})
+
 export default class extends Component {
   render() {
     return (
-      <div className='App' >
-        <Router>
-          <Navbar />
-          <div className='container' >
-            <Switch>
-              <Route exact path='/' component={home} />
-              <Route exact path='/login' component={login} />
-              <Route exact path='/signup' component={signup} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <ThemeProvider theme={theme} >
+        <div className='App' >
+          <Router>
+            <Navbar />
+            <div className='container' >
+              <Switch>
+                <Route exact path='/' component={home} />
+                <Route exact path='/login' component={login} />
+                <Route exact path='/signup' component={signup} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </ThemeProvider>
     )
   }
 }
