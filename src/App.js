e218f1d0cfdd46a19b53567e95c4c3bd00a8a7f2
Natsuki <nthr7286@gmail.com>
2020-02-07 15:30:13 +0900

    Finish Lesson 14 getting Started With React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import { Navbar } from './components'
+import {
+  home,
+  login,
+  signup
+} from './pages'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class extends Component {
+  render() {
+    return (
+      <div className='App' >
+        <Router>
+          <Navbar />
+          <div className='container' >
+            <Switch>
+              <Route exact path='/' component={home} />
+              <Route exact path='/login' component={login} />
+              <Route exact path='/signup' component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+import home from './home'
+import login from './login'
+import signup from './signup'
+
+export { home, login, signup }
